9c6e0fc399d470713604c56fe179158ce66cdb30
sasso-gonzalez <assogonzalezs@gmail.com>
2023-10-12 11:03:17 -0400

    made changes to html and a little js

--- home.js
+++ home.js
@@ -1,8 +1,16 @@
-function numbers(){
 
-}
 
-function operations(){
+// function numbers(){
+//     if(value === "1"){
+//         a = document.getElementById("result").innerHTML += 1;
+//     }
+
+// }
+
+// function operations(){
+
+// }
+function calculate(){
 
 }
 
