a8944a606d2469c75b224b6a974e8831fa7562b0
ZAK0236 <zknight4075@stevenscollege.edu>
2023-10-13 10:04:13 -0400

    Completed JS Base Function

--- home.js
+++ home.js
@@ -1,32 +1,20 @@
-
-
-// function numbers(){
-//     if(value === "1"){
-//         a = document.getElementById("result").innerHTML += 1;
+// function (){
+//     if (text === '.' && value.includes('.')) return;//Won't allow multiple decimals
+//     if (this.value === '') return
+//     if (this.value !== ''){
+//         this.eval()//If the problem need to be done with multiple numbers it will add them and provide the answer while calculating
 //     }
-
+//   }
 // }
 
-// function operations(){
-
-// }
-function calculate(){
-    // x = document.getElementById("result").value
-    // let operation = x.value    //what i was working on to actually calculate problems -serena
-
-function appendNumber(number){
-        if (value === '.' && value.includes('.')) return;//Won't allow multiple decimals
+function calculate (){
+    const result = document.getElementById('result')
+    try {
+        result.value = eval(result.value);
     }
-
-    if (this.value === '') return
-    if (this.value !== ''){
-        this.compute()//If the problem need to be done with multiple numbers it will add them and provide the answer while calculating
+    catch(error){
+        result.value = '${result.value}';
     }
-}
-
-function clear(){
-document.getElementById("result").value=""
-
 }
 
 function back(){
